21d90fa2cf508e8e7b33fee87413f63da15690ca
marki2121 <mmaruna2013@gmail.com>
2020-10-27 19:05:54 +0100

    second commit

---
+++ Python-komba/zd2-1.py
@@ -0,0 +1,9 @@
+"""
+Napravite u Pythonu primjer za dokaz teorema o broju r-kombinacija
+n članog skupa. Uzmite da je n = 4, r = 3. Ispišite skupove A i ,B i i
+provjerite je li T = ∪ i B i .
+"""
+
+n = 4
+r = 3
+
